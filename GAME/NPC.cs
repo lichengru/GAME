e43bac1922f3945a45ce973e7eb46bb2d3ae595c
ivanhii <ivanhii97@gmail.com>
2019-10-31 20:36:42 +0800

    GAME

---
+++ GAME/NPC.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GAME
+{
+    public class NPC:Avatar
+    {
+        private void Talk()
+        {
+            Console.WriteLine("你好");
+        }
+    }
+}
